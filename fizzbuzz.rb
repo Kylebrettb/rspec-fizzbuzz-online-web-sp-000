fccaccf50e0fea2f357b609dfa21b29c1da7c3fa
Kyle  Beckstead <kyle21beckstead@gmail.com>
2018-10-20 02:34:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,4 @@
-# Don't forget! This file needs to be 'required' in its spec fi
-# See README.md for instructions on how to do this
-def fizzbuzz(int)
-if int % 3 == 0 && int % 5 == 0
-else int % 5 == 0!
-elsif int || 
-end
+
 
 
 
